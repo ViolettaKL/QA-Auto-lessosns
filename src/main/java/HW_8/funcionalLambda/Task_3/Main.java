1f88ba61194a26159a2a9d07f5d4a30caa866df6
vrkisdyush <vrkisdyush>
2025-05-09 02:48:41 +0400

    HW_8.Lambda and Stream API

---
+++ src/main/java/HW_8/funcionalLambda/Task_3/Main.java
@@ -0,0 +1,13 @@
+package HW_8.funcionalLambda.Task_3;
+import java.util.function.Predicate;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Predicate<Integer> isEven = number -> number % 2 == 0;
+
+        System.out.println(isEven.test(6));
+        System.out.println(isEven.test(5));
+        System.out.println(isEven.test(3));
+    }
+}
