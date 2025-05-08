e0e526bad45e3bee5e80c9a288d72b8751a90180
vrkisdyush <vrkisdyush>
2025-05-09 02:31:31 +0400

    HW_8.Lambda and Stream API

--- src/main/java/HW_8/funcionalLambda/Task_2/Main.java
+++ src/main/java/HW_8/funcionalLambda/Task_2/Main.java
@@ -1,4 +1,13 @@
 package HW_8.funcionalLambda.Task_2;
 
 public class Main {
+    public static void main(String[] args) {
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Hello from anonymous class!");
+            }
+        };
+        r.run();
+    }
 }
